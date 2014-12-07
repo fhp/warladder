ee929a73ddb6a0ad25ab4259573e061d2fe7a26c
Ruud Koolen <redlizard@redlizard.nl>
2014-12-08 00:45:42 +0100

    todo verwijderd

--- mysettings.php
+++ mysettings.php
@@ -100,7 +100,6 @@ if ($ladderID !== null) {
 	
 	$ladderInfo = db()->stdGet("ladders", array("ladderID"=>$ladderID), array("minSimultaneousGames", "maxSimultaneousGames"));
 	
-	// TODO: melden welke templates je kan spelen.
 	$templates = array(array("type"=>"html", "html"=>"Select your preferred templates. If possible, created games will use one of those templates."));
 	foreach(db()->stdList("ladderTemplates", array("ladderID"=>$ladderID), array("templateID", "name", "warlightTemplateID")) as $template) {
 		$templates[] = array("type"=>"checkbox", "name"=>"template-" . $template["templateID"], "label"=>$template["name"] . " <a href=\"http://warlight.net/MultiPlayer?TemplateID={$template["warlightTemplateID"]}\" target=\"_new\"><em>View on warlight</em></a>" . (db()->stdGet("playerLadderTemplates", array("templateID"=>$template["templateID"]), "canPlay") ? "" : " (Your WarLight level is not high enough to play this template.)"));
